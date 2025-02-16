a248f7f6a469c62c8703fab92b510038e3ff59ff
Nahaq789 <naha022677@icloud.com>
2025-02-17 08:25:09 +0900

    0217

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +11,8 @@ import (
 	"time"
 
 	"github.com/auth-core/cmd/conf"
+	"github.com/auth-core/cmd/di"
+	"github.com/auth-core/pkg/db"
 	"github.com/gin-gonic/gin"
 )
 
@@ -48,4 +51,19 @@ func main() {
 	}
 
 	<-ctx.Done()
+}
+
+func Routing(r gin.IRouter, settng *conf.AppSetting) error {
+	v1 := r.Group("/api/v1")
+	client, err := db.NewDynamoDbClient(context.Background(), settng.Aws.Region)
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+
+	cr := di.Initialize(client)
+	{
+		v1.POST("/signup", cr.AuthController.Signup)
+	}
+
+	return nil
 }
--- cmd/api/routing.go
+++ cmd/api/routing.go
@@ -1,26 +1,27 @@
 package main
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/auth-core/cmd/conf"
-	"github.com/auth-core/cmd/di"
-	"github.com/auth-core/pkg/db"
-	"github.com/gin-gonic/gin"
-)
-
-func Routing(r gin.IRouter, settng *conf.AppSetting) error {
-	v1 := r.Group("/api/v1")
-	client, err := db.NewDynamoDbClient(context.Background(), settng.Aws.Region)
-	if err != nil {
-		return fmt.Errorf("%s", err)
-	}
-
-	cr := di.Initialize(client)
-	{
-		v1.POST("/signup", cr.AuthController.Signup)
-	}
-
-	return nil
-}
+//
+// import (
+// 	"context"
+// 	"fmt"
+//
+// 	"github.com/auth-core/cmd/conf"
+// 	"github.com/auth-core/cmd/di"
+// 	"github.com/auth-core/pkg/db"
+// 	"github.com/gin-gonic/gin"
+// )
+//
+// func Routing(r gin.IRouter, settng *conf.AppSetting) error {
+// 	v1 := r.Group("/api/v1")
+// 	client, err := db.NewDynamoDbClient(context.Background(), settng.Aws.Region)
+// 	if err != nil {
+// 		return fmt.Errorf("%s", err)
+// 	}
+//
+// 	cr := di.Initialize(client)
+// 	{
+// 		v1.POST("/signup", cr.AuthController.Signup)
+// 	}
+//
+// 	return nil
+// }
