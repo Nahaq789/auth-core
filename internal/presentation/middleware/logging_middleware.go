efe769c2bb459c45bb9338f1bbe91e23b581cf5e
Nahaq789 <naha022677@icloud.com>
2025-03-04 08:19:02 +0900

    logging middleware

--- internal/presentation/middleware/logging_middleware.go
+++ internal/presentation/middleware/logging_middleware.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"log/slog"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,9 +12,58 @@ var (
 	timeformat         = "2006/1/2 15:04:05.000 JTS"
 )
 
-func LoggingMiddleware() gin.HandlerFunc {
+func LoggingMiddleware(logger *slog.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		jst, err := time.LoadLocation("Asia/Tokyo")
+		if err != nil {
+			logger.Error("Failed to load location for Asia/Tokyo", "error", err)
+		}
+		start := time.Now().In(jst)
+		startStr := start.Format(timeformat)
+		path := ctx.Request.URL.Path
+		query := ctx.Request.URL.RawQuery
+		method := ctx.Request.Method
+		clientIP := ctx.ClientIP()
+		userAgent := ctx.Request.UserAgent()
+		requestID := ctx.GetHeader(requestIdHeaderKey)
+
+		params := map[string]string{}
+		for _, p := range ctx.Params {
+			params[p.Key] = p.Value
+		}
+
+		loggerWithRequestID := logger.With("RequestID", requestID)
+
+		requestAttr := []slog.Attr{
+			slog.String("Time", startStr),
+			slog.String("Method", method),
+			slog.String("Path", path),
+			slog.String("Query", query),
+			slog.Any("Params", params),
+			slog.String("ClientIP", clientIP),
+			slog.String("User Agent", userAgent),
+		}
+
+		loggerWithRequestID.LogAttrs(
+			ctx.Request.Context(),
+			slog.LevelInfo,
+			"Request Log",
+			requestAttr...,
+		)
 
 		ctx.Next()
+
+		end := time.Now().In(jst)
+		endStr := end.Format(timeformat)
+		latency := end.Sub(start)
+		status := ctx.Writer.Status()
+
 	}
+}
+
+func determineLogLevel(status int) slog.Level {
+	if status == 200 {
+		return slog.LevelInfo
+	}
+	return slog.LevelError
 }
