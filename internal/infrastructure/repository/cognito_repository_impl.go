a6bd4c30ca8c3615f12a8243e7370aa7dca29651
Nahaq789 <naha022677@icloud.com>
2025-03-13 08:22:16 +0900

    fix: repository

--- internal/infrastructure/repository/cognito_repository_impl.go
+++ internal/infrastructure/repository/cognito_repository_impl.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/auth-core/internal/domain/auth"
+	"github.com/auth-core/internal/domain/models/auth"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
@@ -23,24 +23,29 @@ func NewCognitoRepository(client *cognitoidentityprovider.Client, clientId strin
 	}
 }
 
-func (actor *CognitoRepositoryImpl) SignUp(ctx context.Context, auth *auth.Auth) (bool, error) {
+func (actor *CognitoRepositoryImpl) SignUp(ctx context.Context, a *auth.Auth) (*auth.SignUpResult, error) {
 	output, err := actor.CognitoClient.SignUp(ctx, &cognitoidentityprovider.SignUpInput{
 		ClientId: aws.String(actor.clientId),
-		Password: aws.String(auth.Password().String()),
-		Username: aws.String(auth.Email().String()),
+		Password: aws.String(a.Password().String()),
+		Username: aws.String(a.Email().String()),
 		UserAttributes: []types.AttributeType{
-			{Name: aws.String("email"), Value: aws.String(auth.Email().String())},
+			{Name: aws.String("email"), Value: aws.String(a.Email().String())},
 		},
 	})
 
 	if err != nil {
 		var invalidPassword *types.InvalidPasswordException
 		if errors.As(err, &invalidPassword) {
-			return output.UserConfirmed, fmt.Errorf("%s", *invalidPassword.Message)
+			return nil, fmt.Errorf("%s", *invalidPassword.Message)
 		} else {
-			return output.UserConfirmed, fmt.Errorf("Couldn't sign up user %v. message: %v\n", auth.Email().String(), err)
+			return nil, fmt.Errorf("Couldn't sign up user %v. message: %v\n", a.Email().String(), err)
 		}
 	}
 
-	return output.UserConfirmed, err
+	result, err := auth.NewSignUpResult(*output.UserSub)
+	if err != nil {
+		return nil, fmt.Errorf("%s", err)
+	}
+
+	return *&result, err
 }
--- internal/infrastructure/repository/user_repository_impl.go
+++ internal/infrastructure/repository/user_repository_impl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/auth-core/internal/domain/user"
+	"github.com/auth-core/internal/domain/models/user"
 	"github.com/auth-core/internal/infrastructure/mapper"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
