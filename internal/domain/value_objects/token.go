5f5c3d4a4e47fdd691c3539e1d1e493863184901
Nahaq789 <naha022677@icloud.com>
2025-03-22 12:03:53 +0900

    feat: token model

---
+++ internal/domain/value_objects/token.go
@@ -0,0 +1,28 @@
+package valueObjects
+
+import "fmt"
+
+type Token struct {
+	accessToken  string
+	idToken      string
+	refreshToken string
+}
+
+func NewToken(a string, i string, r string) (*Token, error) {
+	err := validateToken(a, i, r)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+	return &Token{
+		accessToken:  a,
+		idToken:      i,
+		refreshToken: r,
+	}, nil
+}
+
+func validateToken(a string, i string, r string) error {
+	if a == "" || i == "" || r == "" {
+		return fmt.Errorf("need all tokens.")
+	}
+	return nil
+}
